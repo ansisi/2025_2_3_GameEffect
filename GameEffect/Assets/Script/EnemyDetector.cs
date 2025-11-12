94e8c3beda7bdb6359f4a344d674c7b5b594192d
ansisi <sihyeona73@gmail.com>
2025-11-12 12:05:30 +0900

    [feat] 쳉니수정

---
+++ GameEffect/Assets/Script/EnemyDetector.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDetector : MonoBehaviour
+{
+    [SerializeField] private float detectionradius = 10.0f;
+    [SerializeField] private LayerMask enemyLayer;
+
+
+    public GameObject GetClosestEnemy()
+    {
+        Collider[] enemiesInRange = Physics.OverlapSphere(transform.position, detectionradius, enemyLayer);
+        if (enemiesInRange.Length > 0)
+        {
+            GameObject bestTarget = null;
+            float closetDistortionSqr = Mathf.Infinity;
+            Vector3 currentPosition = transform.position;
+
+            foreach (Collider enemyCollider in enemiesInRange)
+            {
+                if (enemyCollider.gameObject == this)
+                    continue;
+
+                Vector3 directionToTarget = enemyCollider.transform.position - currentPosition;
+                float dSqrToTarget = directionToTarget.sqrMagnitude;
+
+                if (dSqrToTarget < closetDistortionSqr)
+                {
+                    closetDistortionSqr = dSqrToTarget;
+                    bestTarget = enemyCollider.gameObject;
+                }
+
+            }
+            return bestTarget;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public List<GameObject> GetEnemiesInRange()
+    {
+        List<GameObject> enemiesList = new List<GameObject>();
+        Collider[] enemiesInRange = Physics.OverlapSphere(transform.position, detectionradius, enemyLayer);
+
+        foreach (Collider enemyCollider in enemiesInRange)
+        {
+            if (enemyCollider.gameObject != this.gameObject)
+            {
+                enemiesList.Add(enemyCollider.gameObject);
+            }
+        }
+        return enemiesList;
+    }
+}
